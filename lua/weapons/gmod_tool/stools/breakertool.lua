0134af7f97a6454c87538d0d9f759c3d0a7e5975
Meteor <55365852+MeteorTheLizard@users.noreply.github.com>
2024-07-28 19:03:53 +0200

    Major update
    The tool has received a complete overhaul for better stability and performance. Several bugs have been fixed in the process.

--- lua/weapons/gmod_tool/stools/breakertool.lua
+++ lua/weapons/gmod_tool/stools/breakertool.lua
@@ -1,18 +1,7 @@
+-- Provided by the UBox team. https://ubox.meteorthelizard.com
+
 TOOL.Category = "Construction"
 TOOL.Name = "#tool.breakertool.name"
-
-if CLIENT then
-	function TOOL.BuildCPanel(CPanel)
-		CPanel:AddControl("Header",{
-			Description = "#Tool.breakertool.desc"
-		})
-	end
-
-	language.Add("Tool.breakertool.name","Breaker")
-	language.Add("Tool.breakertool.desc","It's like the Remover except it breaks stuff!")
-	language.Add("Tool.breakertool.left","Primary: Break an Object")
-	language.Add("Tool.breakertool.right","Secondary: Annihilate an Object")
-end
 
 TOOL.Information = {
 	{
@@ -23,58 +12,97 @@ TOOL.Information = {
 	}
 }
 
-local effectData = EffectData() -- We do not have to re-create this
-local damageInfo = DamageInfo()
-damageInfo:SetDamageType(DMG_DIRECT)
 
-local function DoBreak(tTrace,bMode,Ply)
-	local Ent = tTrace.Entity
-	if not IsValid(Ply) or not IsValid(Ent) or Ent == game.GetWorld() or Ent:IsPlayer() then
-		return false
+if CLIENT then
+	language.Add("Tool.breakertool.name","Breaker")
+	language.Add("Tool.breakertool.desc","It's like the remover except it breaks stuff!")
+	language.Add("Tool.breakertool.left","Primary: Break an entity")
+	language.Add("Tool.breakertool.right","Secondary: Annihilate an entity")
+
+	function TOOL.BuildCPanel(CPanel)
+		CPanel:AddControl("Header",{
+			Description = "#Tool.breakertool.desc"
+		})
+
+		CPanel:AddControl("Label",{
+			Text = "Provided by:\n\nThe UBox team.\nhttps://ubox.meteorthelizard.com"
+		})
 	end
+end
+
+
+local fBreakEntity = function(tTrace,bMode,ePly)
+
+	local eEnt = tTrace.Entity
+	if not IsValid(eEnt) or not IsValid(ePly) or eEnt == game.GetWorld() or eEnt:IsPlayer() then
+		return
+	end
+
 
 	if bMode then
-		effectData:SetOrigin(tTrace.HitPos)
-		util.Effect("Explosion",effectData)
+		local obj_EffectData = EffectData()
+		obj_EffectData:SetOrigin(tTrace.HitPos)
+		util.Effect("Explosion",obj_EffectData)
 	end
 
-	if SERVER then
-		damageInfo:SetAttacker(Ply)
-		damageInfo:SetInflictor(IsValid(Ply:GetActiveWeapon()) and Ply:GetActiveWeapon() or Ply)
-		damageInfo:SetDamage(Ent:Health() or 1e9)
-		Ent:TakeDamageInfo(damageInfo)
 
-		if Ent.TriggerInput and string.Left(Ent:GetClass(),2) == "gb" then -- GBombs Compat
-			Ent.MaxDelay = 0 -- Explode instantly
-			Ent:TriggerInput("Detonate",1)
-		end
+	if not SERVER then return true end
 
-		timer.Simple(0,function() -- One tick delay
-			if not IsValid(Ent) then return end
-			Ent:Fire("break",1) -- If applying damage did not work we try to call the internal break function
 
-			timer.Simple(0.1,function() -- If that did not work we simply remove it now
-				if not IsValid(Ent) then return end
-				Ent:Remove()
-			end)
-		end)
+	-- Apply damage to entity to make it break / explode / die
+
+	local obj_DamageInfo = DamageInfo()
+	obj_DamageInfo:SetDamageType(DMG_DIRECT)
+	obj_DamageInfo:SetAttacker(ePly)
+	obj_DamageInfo:SetInflictor(IsValid(ePly:GetActiveWeapon()) and ePly:GetActiveWeapon() or ePly)
+	obj_DamageInfo:SetDamage(1e9)
+	obj_DamageInfo:SetDamageBonus(1e9)
+	obj_DamageInfo:SetDamageForce((eEnt:GetPos() - ePly:GetPos()) * (not bMode and 1000 or 1000000))
+	obj_DamageInfo:SetDamagePosition(tTrace.HitPos)
+	obj_DamageInfo:SetReportedPosition(tTrace.HitPos)
+
+	for I = 1,10 do -- Make absolutely sure this kills the entity
+		eEnt:TakeDamageInfo(obj_DamageInfo)
 	end
+
+
+	-- GBombs Compat
+
+	if eEnt.TriggerInput and (eEnt:GetClass()):sub(1,2) == "gb" then
+		eEnt.MaxDelay = 0 -- Explode instantly
+		eEnt:TriggerInput("Detonate",1)
+	end
+
+
+	timer.Simple(0,function()
+		if not IsValid(eEnt) or not IsValid(ePly) then return end
+
+		SafeRemoveEntityDelayed(eEnt,1) -- If it remains, just remove it after 1 second
+
+		eEnt:Fire("break",1) -- Attempt engine-level break
+
+	end)
+
 
 	return true
+
 end
+
 
 function TOOL:LeftClick(tTrace)
-	if DoBreak(tTrace,false,self:GetOwner()) then return true end
+	if fBreakEntity(tTrace,false,self:GetOwner()) then return true end
 
 	return false
 end
+
 
 function TOOL:RightClick(tTrace)
-	if DoBreak(tTrace,true,self:GetOwner()) then return true end
+	if fBreakEntity(tTrace,true,self:GetOwner()) then return true end
 
 	return false
 end
 
-function TOOL:Reload()
 
+function TOOL:Reload()
+	return false
 end
